964b0fd48d33b3bd8d6208b74e956c5ddc211567
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-12-10 19:44:43 +0300

    Correct name for checkbox

--- src/test/java/ru/netology/CardOrder.java
+++ src/test/java/ru/netology/CardOrder.java
@@ -39,11 +39,11 @@ public class CardOrder {
 
     @Test
     public void shouldSetForm() {
-        driver.get("http://localhost:7777");
+        driver.get("http://localhost:9999");
         System.out.print("");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ордов Олег");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998887766");
-        driver.findElement(By.className("check__box")).click();
+        driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
